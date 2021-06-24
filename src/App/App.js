06b6afa99baeee3b4641845df0cdb29028763336
David Sapp <>
2021-06-24 04:41:23 -0600

    add responsive styles and css variables

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import './App.css';
+import './App.scss';
 
 import HomePage from '../pages/homepage/homepage.component';
 import ShopPage from '../pages/shop/shop.component';
--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
+import './stripe-button.styles.scss';
+
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
