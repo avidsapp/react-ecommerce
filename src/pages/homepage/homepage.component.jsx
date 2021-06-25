27c3ef3d54ba9e54eb17b9f45232e00b71f4d1c5
David Sapp <>
2021-06-24 20:56:10 -0600

    remove Thrown Error for testing on homepage component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,17 +4,10 @@ import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';
 
-const HomePage = () => {
-  throw Error;
-  return (
-    <div></div>
-  );
-};
-
-// const HomePage = () => (
-//   <div className='homepage'>
-//     <Directory />
-//   </div>
-// );
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
 
 export default HomePage;
